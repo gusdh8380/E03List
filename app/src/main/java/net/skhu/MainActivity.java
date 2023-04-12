d1e71f58d94dcecc089a129b43a521c24c3574e8
hyeon5 <gusdh2510@gmail.com>
2023-04-12 14:39:31 +0900

    RecyclerView 객체 추가 RecyclerView Adpater 구현

--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ListViewActivity.class);
         startActivity(intent);
     }
+
+    public void btnRecyclerView1_clicked(View view) {
+        Intent intent = new Intent(this, RecyclerView1Activity.class);
+        startActivity(intent);
+    }
 }
